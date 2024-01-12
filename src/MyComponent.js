c2abd2ad3c071b2ad92f5388c3e0e7ac252d3ee7
Mariia Zaitseva <zaitcevamasha39@gmail.com>
2024-01-12 14:45:32 +0000

    Added iframe

---
+++ src/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MyComponent() {
+    return (
+        <div>
+            <p style={{ color: "red" }}>Testing to see if my component renders!</p>
+        </div>
+    );
+}
+
+export default MyComponent;
