921a81968ad74b61f7d3761997f32d171663997c
Mariia Zaitseva <zaitcevamasha39@gmail.com>
2024-01-15 12:22:50 +0000

    removed components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Frame from 'react-frame-component';
-import MyComponent from "./MyComponent";
+import React from "react";
 
 function App() {
     return (
         <div className='App'>
             <p>Iframes in React</p>
-            <Frame >
-                <MyComponent />
+            <Frame>
+                <p style={{color: "red"}}>Testing to see if my component renders!</p>
             </Frame>
         </div>
     );
